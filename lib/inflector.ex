b72a8f4a2b5708a29f58022c53dbdef15a205bf6
nathanfaucett <nathanfaucett@gmail.com>
2016-05-10 14:57:38 -0500

    fixed recur func

--- lib/inflector.ex
+++ lib/inflector.ex
@@ -11,15 +11,15 @@ defmodule Inflector do
     def create(), do: new()
 
     def uncountable(inflector, value) do
-        Map.put(inflector, :uncountables, [value] ++ inflector.uncountables)
+        Map.put(inflector, :uncountables, inflector.uncountables ++ [value])
     end
 
     def plural(inflector, rule, replacement) do
-        Map.put(inflector, :plurals, [[rule, replacement]] ++ inflector.plurals)
+        Map.put(inflector, :plurals, inflector.plurals ++ [[rule, replacement]])
     end
 
     def singular(inflector, rule, replacement) do
-        Map.put(inflector, :singulars, [[rule, replacement]] ++ inflector.singulars)
+        Map.put(inflector, :singulars, inflector.singulars ++ [[rule, replacement]])
     end
 
     def irregular(inflector, singular, plural) do
@@ -34,6 +34,7 @@ defmodule Inflector do
     def pluralize(inflector, word) do
         replace(inflector.uncountables, inflector.plurals, word)
     end
+
     def is_plural(inflector, word) do
         singularize(inflector, word) != word
     end
@@ -41,33 +42,31 @@ defmodule Inflector do
     def singularize(inflector, word) do
         replace(inflector.uncountables, inflector.singulars, word)
     end
+
     def is_singular(inflector, word) do
         pluralize(inflector, word) != word
     end
 
-    def replace(uncountables, rules, word) do
+    defp replace(uncountables, rules, word) do
         downcase_word = String.downcase(word)
 
         if Enum.find_index(uncountables, fn(value) -> value == downcase_word end) != nil do
             word
         else
-            recur_replace(word, rules, 0, length(rules))
+            recur_replace(word, rules, length(rules))
         end
     end
-    def recur_replace(word, rules, index, length) do
-        pattern = Enum.at(rules, index)
+
+    defp recur_replace(word, rules, 0), do: word
+    defp recur_replace(word, rules, index) do
+        i = index - 1
+        pattern = Enum.at(rules, i)
         rule = Enum.at(pattern, 0)
 
         if Regex.match?(rule, word) do
             Regex.replace(rule, word, Enum.at(pattern, 1))
         else
-            index = index + 1
-
-            if index < length do
-                recur_replace(word, rules, index, length)
-            else
-                word
-            end
+            recur_replace(word, rules, i)
         end
     end
 end
